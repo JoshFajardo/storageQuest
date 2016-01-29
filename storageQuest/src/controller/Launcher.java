b1d25eeff3b69497de3990ed4cbe38872a10e961
Fajosh <Fajosh@Fajosh-PC>
2016-01-28 23:59:39 -0600

    created warehouse models

--- storageQuest/src/controller/Launcher.java
+++ storageQuest/src/controller/Launcher.java
@@ -1,8 +1,22 @@
 package controller;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import models.Warehouse;
+import models.WarehouseList;
+/*later will need to import the models and views*/
+
+
+//starts the whole thing
 public class Launcher {
 
-	
+	public static void createAndShowGIU(){
+		
+		
+		
+		
+	}
 	
 	
 	
