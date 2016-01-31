e1ec3bcf49c759b7ce3a767fe53e7bfcd2832836
Fajosh <Fajosh@Fajosh-PC>
2016-01-30 22:51:33 -0600

    Finished the warehouse model, and warehouseList model

--- storageQuest/src/models/Warehouse.java
+++ storageQuest/src/models/Warehouse.java
@@ -33,7 +33,7 @@ public class Warehouse extends Observable{
 		storageCapacity = 0;
 	}
 	
-	public Warehouse (String wn, String ad, String ct, String st, String zp, int sc){
+	public Warehouse (String wn, String ad, String ct, String st, String zip, int sc){
 		this ();
 		if(!validWarehouseName(wn))
 			throw new IllegalArgumentException("Invalid warehouse name!");
@@ -43,29 +43,168 @@ public class Warehouse extends Observable{
 			throw new IllegalArgumentException("Invalid city!");
 		if(!validState(st))
 			throw new IllegalArgumentException("Invalid State!");
-		
-		
+		if(!validZip(zip))
+			throw new IllegalArgumentException("Invalid Zip!");
+		if(!validStorageCapacity(sc))
+			throw new IllegalArgumentException("Invalid Storage Capacity!");
+		warehouseName = wn;
+		address = ad;
+		city = ct;
+		state = st;
+		this.zip = zip;
+		storageCapacity = sc;
+			
 	}
-
-	public boolean validState(String st) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public long getId(){
+		return id;
 	}
-
-	public boolean validCity(String ct) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	public String getWarehouseName(){
+		return warehouseName;
 	}
-
-	public boolean validAddress(String ad) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	
 	public boolean validWarehouseName(String wn) {
-		// TODO Auto-generated method stub
-		return false;
+		if (wn == null || wn.isEmpty())
+			return false;
+		if (wn.length() > 255)
+			return false;
+		return true;
 	}
+
+	public void setWarehouseName(String warehouseName){
+		if(!validWarehouseName(warehouseName))
+			throw new IllegalArgumentException("Invalid warehouse name");
+		this.warehouseName = warehouseName;
+		//lets the observer know of the change
+		setChanged();
+	}
+	
+	public String getAddress(){
+		return address;
+	}
+	public boolean validAddress(String ad) {
+		if (ad == null || ad.isEmpty())
+			return false;
+		if (ad.length()> 255)
+			return false;
+		return true;
+	}
+	
+	public void setAddress(String address){
+		if(!validAddress(address))
+			throw new IllegalArgumentException("Invalid address!");
+		this.address = address;
+		//observer notification
+		setChanged();
+		
+	}
+	
+	public String getCity() {
+		return city;
+	}
+	
+	public boolean validCity(String ct) {
+		if ( ct == null || ct == "")
+			return false;
+		if (ct.length()> 100)
+			return false;
+		return true;
+	}
+	
+	public void setCity(String city){
+		if(!validCity(city))
+			throw new IllegalArgumentException("Invalid city!");
+		this.city = city;
+		//observer notification
+		setChanged();
+	}
+	
+	public String getState(){
+		return state;
+	}
+	
+	public boolean validState(String st) {
+		if ( st == null || st == "")
+			return false;
+		if (st.length()> 50)
+			return false;
+		return true;
+	}
+	
+	public void setState(String state){
+		if(!validState(state))
+			throw new IllegalArgumentException("Invalid state!");
+		this.state = state;
+		//observer notification
+		setChanged();
+	}
+	
+	public String getZip(){
+		return zip;
+	}
+	
+	public boolean validZip(String zip) {
+		if (zip.contains("[a-zA-z]+"))
+			return false;
+		if (zip.length() != 5)
+			return false;
+		return true;
+	}
+	
+	public void setZip(String zip){
+		if(!validZip(zip))
+			throw new IllegalArgumentException("Invalid zip!");
+		this.zip = zip;
+		//observer notification
+		setChanged();
+	}
+	
+	public Integer getStorageCapacity(){
+		return storageCapacity;
+	}
+	
+	public boolean validStorageCapacity(Integer sc) {
+		if (sc < 0)
+			return false;
+		return true;
+	}
+
+	public void setStorageCapacity(Integer storageCapacity){
+		if(!validStorageCapacity(storageCapacity))
+			throw new IllegalArgumentException("Invalid storage capacity!");
+		this.storageCapacity = storageCapacity;
+		//observer notification
+		setChanged();
+	}
+	
+
+	public void finishUpdate() {
+		notifyObservers();
+	}
+	
+	
+	
+	
+	@Override
+	public String toString(){
+		String fullName = getWarehouseName();
+		
+		fullName += " Address: " + address;
+		fullName += " City: " + city;
+		fullName += " State: " + state;
+		fullName += " Zip: " + zip;
+		fullName += " Storage Capacity: " + storageCapacity;
+		
+		return fullName;
+		
+	}
+
+	
+
+	
+
+	
 	
 	
 }
--- storageQuest/src/models/WarehouseList.java
+++ storageQuest/src/models/WarehouseList.java
@@ -1,5 +1,30 @@
 package models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class WarehouseList {
 
+	private List<Warehouse> myList;
+	
+	public WarehouseList(){
+		myList = new ArrayList<Warehouse>();
+	}
+	public void addWarehouseToList(Warehouse w){
+		myList.add(w);
+	}
+	
+	public Warehouse removeWarehouseFromList(Warehouse w){
+		if(myList.contains(w)){
+			myList.remove(w);
+			return w;
+		}
+		return null;
+	}
+	public List<Warehouse>getList(){
+		return myList;
+	}
+	public void setList(List<Warehouse> myList){
+		this.myList = myList;
+	}
 }
